947fbdb7e26eb433f24cac40e40e51f6c9edb483
Carla Crude <carlacristinecrudedossanto@correio.itau.com.br>
2018-09-15 02:18:45 -0300

    Layout bootstrap

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,10 +8,12 @@ class RecipesController < ApplicationController
     end
 
     def create
-        @recipe = Recipe.new(params.require(:recipe).permit(:title, :recipe_type_id, :cuisine_id, :difficulty, :ingredients, :cook_time, :cook_method, :recipe_image))
+        @recipe = Recipe.new(params.require(:recipe).permit(:title, :recipe_type_id, :cuisine_id, :difficulty, :ingredients, :cook_time, :cook_method, :recipe_image, :featured))
         if @recipe.save
             redirect_to recipe_path(@recipe.id)
+            flash[:success]='Sua receita foi cadastrada com sucesso'
         else
+            flash[:notice]='Você deve informar todos os dados da receita'
             render 'new'
         end
     end
@@ -23,6 +25,10 @@ class RecipesController < ApplicationController
     def update
         @recipe = Recipe.find(params[:id])
         if @recipe.update(params.require(:recipe).permit(:title, :recipe_type_id, :cuisine_id, :difficulty, :ingredients, :cook_time, :cook_method, :recipe_image, :featured))
+            flash[:success]='Sua receita foi editada com sucesso'
+            if !@recipe.featured_before_last_save && @recipe.featured?
+                flash[:success] = 'Receita marcada como destaque com sucesso!'   
+            end
             redirect_to recipe_path(@recipe.id)
         else
             render 'edit'
