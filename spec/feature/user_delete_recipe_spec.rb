3054340417138faf421ac161999c3b14b2d3bcaa
Carla Crude <carlacristinecrudedossanto@correio.itau.com.br>
2018-09-16 19:40:08 -0300

    Excluir uma receita

--- spec/feature/user_delete_recipe_spec.rb
+++ spec/feature/user_delete_recipe_spec.rb
@@ -12,7 +12,24 @@ feature 'User delete recipe' do
 
     visit root_path
     click_on 'Bolo de cenoura'
-    click_on 'Excluir esta receita'
+    click_on 'Excluir'
+
+    expect(page).to have_content('Deseja excluir esta receita?')
+  end
+
+  scenario 'and show a confirmation message' do
+    recipe_type = RecipeType.create(name: 'Sobremesa')
+    cuisine = Cuisine.create(name: 'Brasileira')
+    Recipe.create(title: 'Bolo de cenoura', difficulty: 'Médio',
+                  recipe_type: recipe_type, cuisine: cuisine,
+                  cook_time: 50, ingredients: 'Farinha, açucar, cenoura',
+                  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes', 
+                  recipe_image: File.new(Rails.root.join('spec', 'support', 'fixtures', 'Feijoada.jpg')))
+
+    visit root_path
+    click_on 'Bolo de cenoura'
+    click_on 'Excluir'
+    click_on 'Sim'
 
     expect(page).to have_content('Sua receita foi excluída com sucesso')
     expect(page).not_to have_content('Bolo de cenoura')
